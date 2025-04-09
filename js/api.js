312f9282fea1e9a3a8a83257c83211fb96a6b249
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-04-09 20:31:24 -0300

    feat(pensamentos): add new card from submitted data on form

--- js/api.js
+++ js/api.js
@@ -9,6 +9,23 @@ const api = {
             alert('Erro ao buscar pensamentos')
             throw error;
         }
+    },
+
+    async salvarPensamento(pensamento){
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST", //quero enviar uma informação
+                headers: {
+                    "Content-Type": "application/json" //Estou informando o tipo de conteúdo que estamos enviando nessa requisição
+                },
+                body: JSON.stringify(pensamento) //o corpo da requisição, irá converter nosso objeto JS para string JSON
+            });
+            return await response.json();
+        } 
+        catch {
+            alert('Erro ao enviar pensamento')
+            throw error;
+        }
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById('pensamento-form');
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
 })
+
+async function manipularSubmissaoFormulario(event){
+    event.preventDefault(); // comportamento padrão é atualizar a página
+    const id = document.getElementById("pensamento-id").value;
+    const conteudo = document.getElementById("pensamento-conteudo").value;
+    const autoria = document.getElementById("pensamento-autoria").value;
+
+    try {
+        await api.salvarPensamento({ conteudo, autoria }) // não somos nós que passamos o id, o JSON server vai ficar encarregado de criar automaticamente um id aleatório
+        ui.renderizarPensamentos();
+    }
+    catch {
+        alert("Erro ao salvar os pensamentos");
+        throw error;
+    }
+}
