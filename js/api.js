f35b00e5279c852f38e379f9e8aa913a8ad84c96
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-04-09 19:53:47 -0300

    feat(api): add fetch function

---
+++ js/api.js
@@ -0,0 +1,15 @@
+//Responsável por conter nossas requisições a API
+const api = {
+    async buscarPensamento() {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos');
+            return await response.json();
+        }
+        catch {
+            alert('Erro ao buscar pensamentos')
+            throw error;
+        }
+    }
+}
+
+export default api; //Nos deixa acessar essa constante em outros arquivos js
